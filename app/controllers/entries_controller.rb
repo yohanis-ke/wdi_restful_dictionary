bfb0b644ef3f87ebbd5f988082b9d613f050d526
yohanis-ke <yohanes.ke@gmail.com>
2018-08-10 14:49:02 -0400

    have finished Dictionary Part 1: RESTful Routes

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,24 +1,24 @@
 class EntriesController < ApplicationController
 
-    def index
-       render :index
-    end
+    # def index
+    #    render :index
+    # end
 
-    def show
-      render :show
-    end
-
-    def new
-      render :new
-    end
+    # def show
+    #   render :show
+    # end
+    #
+    # def new
+    #   render :new
+    # end
 
     def create
       redirect_to entries_url
     end
 
-    def edit
-      render :edit
-    end
+    # def edit
+    #   render :edit
+    # end
 
     def update
       redirect_to entry_url(params[:id])
